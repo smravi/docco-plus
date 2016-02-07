f1ed766be4fcf8bf5175821a6aefded8eace7308
Pranav Shekhar Jha <prjha@ebay.com>
2016-02-07 11:42:27 +0530

    added clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,8 @@ module.exports = function(grunt) {
             clean: [
                 '.coverage',
                 '.test',
-                '.cache'
+                '.cache',
+                '.docs'
             ],
             jshint: {
                 lib: {
